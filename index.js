60f4474c1b8f26013b633badb0d938deef664bea
schive11 <22871389+schive11@users.noreply.github.com>
2023-08-11 21:44:53 -0400

    add script without error checking

---
+++ index.js
@@ -0,0 +1,46 @@
+
+const submit = document.querySelector('.link-submit');
+const url = 'https://api.shrtco.de/v2/shorten?url=$'
+const output = document.querySelector('.output');
+
+submit.addEventListener("click", async () => {
+    try{
+        var input_value = document.querySelector('.link-input').value;
+        const response = await fetch(`${url}+${encodeURIComponent(input_value)}`);
+        const data = await response.json();
+        var link = data.result.short_link;
+        var result = `<div class="output-links">
+                                    <div class="input-link">
+                                        <p>${input_value}</p>
+                                    </div>
+                                    <div class="copy">
+                                    <p href="${link}">${link}</p>
+                                    <button class="copy-button">Copy</button>
+                                    </div>
+                         </div>`;
+            
+         document.querySelector('.output').insertAdjacentHTML("afterbegin",result);
+
+        
+    }catch(error){
+        console.error(`Error: ${error.message}`);
+    }
+})
+
+
+document.addEventListener('click', function (event) {
+    if (!event.target.classList.contains('copy-button')) return;
+
+    let short_link = event.target.parentNode.querySelector('.copy > p');
+ 
+    navigator.clipboard.writeText(short_link.innerHTML);
+
+    event.target.classList.add('copied');
+    event.target.textContent = 'Copied!';
+    
+    setTimeout(() => {
+        event.target.classList.remove('copied')
+        event.target.textContent = 'Copy'
+    }, 5000)
+
+})
